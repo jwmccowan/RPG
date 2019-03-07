85c44e989d06cfbb5cc440c0cf0af6375ac7962c
John McCowan <jwmccowan@gmail.com>
2019-03-07 00:40:12 -0800

    added some skellie files and a full Point struct, should come in handy with board creation

---
+++ Assets/Scripts/Extensions/Point.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Point
+{
+    public int x;
+    public int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point operator +(Point a, Point b)
+    {
+        return new Point(a.x + b.x, a.y + b.y);
+    }
+
+    public static Point operator -(Point a, Point b)
+    {
+        return new Point(a.x - b.x, a.y - b.y);
+    }
+
+    public static bool operator ==(Point a, Point b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+
+    public static bool operator !=(Point a, Point b)
+    {
+        return !(a == b);
+    }
+
+    public static implicit operator Vector2(Point p)
+    {
+        return new Vector2(p.x, p.y);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Point)
+        {
+            Point p = (Point)obj;
+            return x == p.x && y == p.y;
+        }
+        return false;
+    }
+
+    public bool Equals(Point p)
+    {
+        return x == p.x && y == p.y;
+    }
+
+    public override int GetHashCode()
+    {
+        return x ^ y;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1})", x, y);
+    }
+}

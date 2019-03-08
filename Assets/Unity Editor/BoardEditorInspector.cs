34b96eec16fcfdad050ce838db130ca4ebba0c82
John McCowan <jwmccowan@gmail.com>
2019-03-08 00:29:09 -0800

    #1: Board Creator
    An inspector script that simply exposes public functions in Board Creator to buttons in the Unity Editor

---
+++ Assets/Unity Editor/BoardEditorInspector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BoardEditor))]
+public class BoardEditorInspector : Editor
+{
+    public BoardEditor current
+    {
+        get
+        {
+            return (BoardEditor)target;
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Clear"))
+            current.Clear();
+        if (GUILayout.Button("Grow"))
+            current.Raise();
+        if (GUILayout.Button("Shrink"))
+            current.Lower();
+        if (GUILayout.Button("Grow Area"))
+            current.RaiseArea();
+        if (GUILayout.Button("Shrink Area"))
+            current.LowerArea();
+        if (GUILayout.Button("Save"))
+            current.Save();
+        if (GUILayout.Button("Load"))
+            current.Load();
+        if (GUI.changed)
+            current.UpdateMarker();
+    }
+}

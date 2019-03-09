a5496be1a4b0b2f13af5f41c7ba92abf51acd6cb
John McCowan <jwmccowan@gmail.com>
2019-03-08 20:46:29 -0800

    #7: A few battle states
    Added an init state which simply initializes the board, and will probably do more in the future.  Also a move target state, which allows you to move the tile selector with the arrow keys

---
+++ Assets/Scripts/Controller/States/BattleStates/InitBattleState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitBattleState : BattleState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        Debug.Log("InitBattleState Enter");
+        StartCoroutine(Init());
+    }
+
+    /// <summary>
+    /// Inits the battle
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Init()
+    {
+        board.Load(levelData);
+        pos = new Point((int)levelData.tilePositions[0].x, (int)levelData.tilePositions[0].z);
+        SelectTile(pos);
+        yield return null;
+        owner.ChangeState<MoveTargetState>();
+    }
+}

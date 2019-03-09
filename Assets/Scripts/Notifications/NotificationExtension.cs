dedfc746ecb6fbd8108002a1aa3e4a448ef8c64f
John McCowan <jwmccowan@gmail.com>
2019-03-08 16:45:00 -0800

    #3: Notification Extension class
    To decouple the notification handler from the clients using it

---
+++ Assets/Scripts/Notifications/NotificationExtension.cs
@@ -0,0 +1,34 @@
+﻿using Handler = System.Action<System.Object, System.Object>;
+
+public static class NotificationExtension
+{
+    public static void PostNotification(this object obj, string notification)
+    {
+        NotificationHandler.instance.PostNotification(notification, obj);
+    }
+
+    public static void PostNotification(this object obj, string notification, object e)
+    {
+        NotificationHandler.instance.PostNotification(notification, obj, e);
+    }
+
+    public static void AddListener(this object obj, Handler handler, string notification)
+    {
+        NotificationHandler.instance.AddListener(handler, notification);
+    }
+
+    public static void AddListener(this object obj, Handler handler, string notification, object sender)
+    {
+        NotificationHandler.instance.AddListener(handler, notification, sender);
+    }
+
+    public static void RemoveListener(this object obj, Handler handler, string notification)
+    {
+        NotificationHandler.instance.AddListener(handler, notification);
+    }
+
+    public static void RemoveListener(this object obj, Handler handler, string notification, object sender)
+    {
+        NotificationHandler.instance.AddListener(handler, notification, sender);
+    }
+}

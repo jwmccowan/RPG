17bf9301289517cd21f038afd78ff8576f0aaa59
John McCowan <jwmccowan@gmail.com>
2019-03-08 18:00:37 -0800

    #4: Added input test

---
+++ Assets/Scripts/Test Scripts/InputTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.AddListener(OnMove, "InputController.MoveEventNotification");
+        this.AddListener(OnFire, "InputController.FireEventNotification");
+    }
+
+    void OnMove(object sender, object e)
+    {
+        Point p = (Point)e;
+        Debug.Log(string.Format("Move Event Received: {0}", e.ToString()));
+    }
+
+    void OnFire(object sender, object e)
+    {
+        int p = (int)e;
+        Debug.Log(string.Format("Fire Event Received: {0}", e.ToString()));
+    }
+}

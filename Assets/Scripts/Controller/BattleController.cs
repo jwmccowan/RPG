440e7316105d25d4f9051f90c690b61c0773cd97
John McCowan <jwmccowan@gmail.com>
2019-03-08 20:02:12 -0800

    #7: Battle Controller
    Added class that will be main controller for battle scene.  Right now we don't have any state, so I commented out a reference to what I believe will be the first state in a battle

---
+++ Assets/Scripts/Controller/BattleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BattleController : StateMachine
+{
+    public CameraRig cameraRig;
+    public Board board;
+    public LevelData levelData;
+    public Transform tileSelectionIndicator;
+    public Point pos;
+
+    private void Start()
+    {
+        //Asuming we'll have something like this
+        //Will remove comment when implemented
+        //ChangeState<InitBattleState>();
+    }
+}

5e7e26bb452c2d8f46413dcccf431cc9e98eb525
John McCowan <jwmccowan@gmail.com>
2019-03-08 19:59:07 -0800

    #7: Board Edits
    The Board class will be responsible for loading levelData as well as keeping track of all the tile and their positions

--- Assets/Scripts/View Model/Board.cs
+++ Assets/Scripts/View Model/Board.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField] GameObject tilePrefab;
+    public Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
+
+    public void Load(LevelData data)
+    {
+        List<Vector3> tilePositions = data.tilePositions;
+        for (int i = 0; i < tilePositions.Count; i++)
+        {
+            GameObject instance = Instantiate(tilePrefab);
+            Tile t = instance.GetComponent<Tile>();
+            t.Load(tilePositions[i]);
+            tiles.Add(t.pos, t);
+        }  
+    }
 }

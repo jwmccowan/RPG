075c23141b13aa2a3ac44705bbb9e26ef4e9d041
John McCowan <jwmccowan@gmail.com>
2019-03-08 20:35:14 -0800

    #7: Battle State
    Added an abstract battle state from which other battle states will inherit.  It is really just there to take central care of functionality and a connection to the Battle Controller's properties.  Right now, it sets up move and fire event listeners and has a select tile function which I assume inheriting states will need

---
+++ Assets/Scripts/Controller/States/BattleStates/BattleState.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleState : State
+{
+    BattleController owner;
+    public CameraRig cameraRig { get { return owner.cameraRig; } }
+    public Board board { get { return owner.board; } }
+    public LevelData levelData { get { return owner.levelData; } }
+    public Transform tileSelectionIndicator { get { return owner.tileSelectionIndicator; } }
+    public Point pos { get { return owner.pos; } set { owner.pos = value; } }
+
+    protected virtual void Awake()
+    {
+        owner = GetComponent<BattleController>();
+    }
+
+    protected override void AddListeners()
+    {
+        this.AddListener(OnMove, InputController.moveEventNotification);
+        this.AddListener(OnFire, InputController.moveEventNotification);
+    }
+
+    protected override void RemoveListeners()
+    {
+        this.RemoveListener(OnMove, InputController.moveEventNotification);
+        this.RemoveListener(OnFire, InputController.moveEventNotification);
+    }
+
+    protected virtual void OnMove(object sender, object e)
+    {
+
+    }
+
+    protected virtual void OnFire(object sender, object e)
+    {
+
+    }
+
+    protected virtual void SelectTile(Point p)
+    {
+        if (pos == p || !board.tiles.ContainsKey(p))
+        {
+            return;
+        }
+
+        pos = p;
+        tileSelectionIndicator.localPosition = board.tiles[p].center;
+    }
+}

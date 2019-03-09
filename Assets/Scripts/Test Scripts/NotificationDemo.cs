a9afaa4e9ffbda2cfc6591d96fd00e200c585b16
John McCowan <jwmccowan@gmail.com>
2019-03-08 17:10:59 -0800

    #3: A test script and scene for notifications

---
+++ Assets/Scripts/Test Scripts/NotificationDemo.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Notifications.Test1
+{
+    public class NotificationDemo : MonoBehaviour
+    {
+        public const string Notification = "Demo.Notification";
+        public int listenerCount = 250;
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                Test();
+            }
+        }
+        void Test()
+        {
+            Listener[] listeners = new Listener[listenerCount];
+            for (int i = 0; i < listenerCount; ++i)
+            {
+                listeners[i] = new Listener();
+                listeners[i].Enable();
+            }
+
+            this.PostNotification(Notification);
+
+            for (int i = 0; i < listeners.Length; ++i)
+                listeners[i].Disable();
+            NotificationHandler.instance.Clean();
+        }
+    }
+
+    public class Listener
+    {
+        public const string Clear = "Listener.Clear";
+
+        public void Enable()
+        {
+            this.AddListener(OnTest, NotificationDemo.Notification);
+            this.AddListener(OnClear, Listener.Clear);
+        }
+
+        public void Disable()
+        {
+            this.RemoveListener(OnTest, NotificationDemo.Notification);
+            this.RemoveListener(OnClear, Listener.Clear);
+        }
+
+        void OnTest(object sender, object info)
+        {
+            Debug.Log("Got the message!");
+            this.PostNotification(Listener.Clear);
+        }
+
+        void OnClear(object sender, object info)
+        {
+            this.RemoveListener(OnTest, NotificationDemo.Notification);
+        }
+    }
+}

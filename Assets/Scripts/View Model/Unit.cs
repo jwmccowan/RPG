d2b923acd7f2441af5815819d28b36c13192887e
John McCowan <jwmccowan@gmail.com>
2019-03-09 16:41:33 -0800

    #12: Unit
    The unit class stores a tile (location) and has a method to place on another tile.  More functionality will be added with more features

---
+++ Assets/Scripts/View Model/Unit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public Tile tile { get; protected set; }
+
+    public void Place(Tile t)
+    {
+        if (tile.content != null)
+        {
+            return;
+        }
+
+        if (tile != null && tile.content == gameObject)
+        {
+            tile = null;
+        }
+
+        tile = t;
+
+        if (tile != null)
+        {
+            transform.position = tile.center;
+        }
+    }
+}

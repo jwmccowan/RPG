246773ad46e11321611c838577fca1f85fd74d14
John McCowan <jwmccowan@gmail.com>
2019-03-09 16:42:25 -0800

    #12: Changes or Unit
    Added a content field to Tile, plus some comments

--- Assets/Scripts/View Model/Tile.cs
+++ Assets/Scripts/View Model/Tile.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// This component stores the coordinates and dimensions of a tile in our game board,
+/// as well as a few helper functions for dealing with them, including way of raising
+/// or lowering its height
+/// </summary>
 public class Tile : MonoBehaviour
 {
     /* 
@@ -9,18 +14,35 @@ public class Tile : MonoBehaviour
     */
 
     #region Fields
-    /* Pos holds the x, y coordinates of the tile
-     * Height is how many steps up the tile is from the base height
-     * But the actual height is Height * StepHeight
-     * I tried messing with ways to get the tile to Match whenever these values were
-     * changed, but this creates issue in Unity editor.  Not too worried, not too many 
-     * clients will change these.
-    */
+    /// <summary>
+    /// The x, y coordinates of the tile
+    /// </summary>
     public Point pos;
-    public int height;
 
+    /// <summary>
+    /// The number of steps up the tile is from base height (0)
+    ///     Actual height of the tile is height * stepHeight
+    /// </summary>
+    public int height
+    {
+        get { return height; }
+        set { height = value; Match(); }
+    }
+
+    /// <summary>
+    /// The gameObject currently standing on the tile
+    ///     As of now, this is a Unit, but this might hold traps, items, w/e in the future
+    /// </summary>
+    public GameObject content;
+
+    /// <summary>
+    /// Height of each 'step'
+    /// </summary>
     public const float stepHeight = 0.25f;
-    // A way for a client to center itself on the tile
+
+    /// <summary>
+    /// The center of the top of a tile
+    /// </summary>
     public Vector3 center
     {
         get
@@ -31,6 +53,11 @@ public class Tile : MonoBehaviour
     #endregion
 
     #region Public
+    /// <summary>
+    /// Set the position and height of the Tile and transform
+    /// </summary>
+    /// <param name="pos">(x, y) coordinates of the tile</param>
+    /// <param name="height">height in stepHeight units of the tile</param>
     public void Load(Point pos, int height)
     {
         this.pos = pos;
@@ -38,26 +65,37 @@ public class Tile : MonoBehaviour
         Match();
     }
 
+    /// <summary>
+    /// Set the position and height of the Tile and transform
+    /// </summary>
+    /// <param name="v">Vector3 coordinates of the tile, with height = v.y</param>
     public void Load(Vector3 v)
     {
         Load(new Point((int)v.x, (int)v.z), (int)v.y);
     }
 
-    // We call this method after changing coordinates to reflect changes in scale/position
+    /// <summary>
+    /// Called after changing the x, y, or height of a Tile, only use in emergency
+    /// </summary>
     public void Match()
     {
         transform.position = new Vector3(pos.x, (height * stepHeight) / 2f, pos.y);
         transform.localScale = new Vector3(1, height * stepHeight, 1);
     }
 
-    // Some methods for the board editor to use to create a board organically
-    public void Grow()
+    /// <summary>
+    /// Raises height by one
+    /// </summary>
+    public void Raise()
     {
         height++;
         Match();
     }
 
-    public void Shrink()
+    /// <summary>
+    /// Lowers height by one
+    /// </summary>
+    public void Lower()
     {
         height--;
         Match();

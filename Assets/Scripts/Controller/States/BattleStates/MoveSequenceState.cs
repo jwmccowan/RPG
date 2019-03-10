48c97afb595f5c4efdcf75cb3960a80d47727b50
John McCowan <jwmccowan@gmail.com>
2019-03-10 01:40:37 -0800

    #9: States
    Added a state for choosing a move location and moving, editing a few more to work with them

---
+++ Assets/Scripts/Controller/States/BattleStates/MoveSequenceState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSequenceState : BattleState
+{
+    public override void Enter()
+    {
+        Debug.Log("InitBattleState Enter");
+        base.Enter();
+        StartCoroutine("Sequence");
+    }
+
+    IEnumerator Sequence()
+    {
+        Movement m = owner.currentUnit.GetComponent<Movement>();
+        yield return m.Traverse(owner.currentTile);
+        owner.ChangeState<SelectUnitState>();
+    }
+}
---
+++ Assets/Scripts/Controller/States/BattleStates/SelectUnitState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectUnitState : BattleState
+{
+    public override void Enter()
+    {
+        Debug.Log("SelectUnitState Enter");
+        base.Enter();
+    }
+
+    protected override void OnMove(object sender, object e)
+    {
+        Point p = (Point)e;
+        SelectTile(pos + p);
+    }
+
+    protected override void OnFire(object sender, object e)
+    {
+        int i = (int)e;
+        GameObject content = owner.currentTile.content;
+        if (content != null)
+        {
+            owner.currentUnit = content.GetComponent<Unit>();
+            owner.ChangeState<MoveTargetState>();
+        }
+    }
+}

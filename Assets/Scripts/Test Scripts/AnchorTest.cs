330c475d7ba2196ec77c0b3395a6c89cd69e8060
John McCowan <jwmccowan@gmail.com>
2019-03-11 15:01:07 -0700

    #17: Test Script and Scene
    test scene with accompanying script to test out our new layout system

---
+++ Assets/Scripts/Test Scripts/AnchorTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorTest : MonoBehaviour
+{
+    [SerializeField] bool animated;
+    [SerializeField] float delay = 0.5f;
+
+    IEnumerator Start()
+    {
+        LayoutAnchor anchor = GetComponent<LayoutAnchor>();
+        while (true)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    TextAnchor a1 = (TextAnchor)i;
+                    TextAnchor a2 = (TextAnchor)j;
+                    Debug.Log(string.Format("A1: {0}, A2: {1}", a1, a2));
+                    if (animated)
+                    {
+                        Tweener t = anchor.MoveToAnchorPosition(a1, a2, Vector2.zero);
+                        while (t != null)
+                        {
+                            yield return null;
+                        }
+                    }
+                    else
+                    {
+                        anchor.SnapToAnchorPosition(a1, a2, Vector2.zero);
+                    }
+                    yield return new WaitForSeconds(delay);
+                }
+            }
+        }
+    }
+}

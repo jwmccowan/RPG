1b7ec8f282acfd6f74bdc5f02e37cd07b4e53e8a
John McCowan <jwmccowan@gmail.com>
2019-03-11 17:22:59 -0700

    #17: Panel Test
    test script to add to panel component that shows how it is used

---
+++ Assets/Scripts/Test Scripts/PanelTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelTest : MonoBehaviour
+{
+    public bool animated;
+
+    Panel panel;
+    const string Show = "Show";
+    const string Hide = "Hide";
+    const string Center = "Center";
+
+    void Start()
+    {
+        panel = GetComponent<Panel>();
+        Panel.Position centerPos = new Panel.Position(Center, TextAnchor.MiddleCenter, TextAnchor.MiddleCenter);
+        panel.AddPosition(centerPos);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 30), Show))
+        {
+            panel.SetPosition(Show, animated);
+        }
+        if (GUI.Button(new Rect(10, 50, 100, 30), Hide))
+        {
+            panel.SetPosition(Hide, animated);
+        }
+        if (GUI.Button(new Rect(10, 90, 100, 30), Center))
+        {
+            Tweener t = panel.SetPosition(Center, animated);
+            if (t != null)
+            {
+                t.easingControl.equation = EasingEquations.EaseInOutBack;
+            }
+        }
+    }
+}

e75068a223685df1f0d59854f043c2faf1ce5141
John McCowan <jwmccowan@gmail.com>
2019-03-19 22:08:42 -0700

    #28: Ability Score!!
    Bonus returns the ability score bonus.  Go figure

---
+++ Assets/Scripts/View Model/Actor/Stats/AbilityScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityScore : Stat
+{
+    public override int bonus
+    {
+        get
+        {
+            return (value - 10) / 2;
+        }
+    }
+
+    public AbilityScore(int value) : base(value) { }
+}

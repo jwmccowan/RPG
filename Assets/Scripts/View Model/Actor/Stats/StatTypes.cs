5c55d3c92adcd1bd69ee7b7d0355455602fa07d2
John McCowan <jwmccowan@gmail.com>
2019-03-19 21:59:17 -0700

    #28: StatTypes
    An enum with a few sample stat types.  As the game progresses, I imagine this list will get pretty long.  And subject to change.

---
+++ Assets/Scripts/View Model/Actor/Stats/StatTypes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatTypes
+{
+    HP,
+    Max_HP,
+    HP_Increases,
+    Strength,
+    Dexterity,
+    Intelligence,
+    AC,
+    Initiative,
+    BAB,
+
+    Count
+}

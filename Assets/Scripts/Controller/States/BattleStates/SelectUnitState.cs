a9a0f77c8000948ef854fa73fe1b852160ce0e85
John McCowan <jwmccowan@gmail.com>
2019-03-19 19:42:06 -0700

    #21: Select Unit State
    cycles through units and selects each one in turn through a IEnumerator.  When it regains control, it just continues the loop on to the next unit forever.  This will be expanded upon when we get initiative in order

--- Assets/Scripts/Controller/States/BattleStates/SelectUnitState.cs
+++ Assets/Scripts/Controller/States/BattleStates/SelectUnitState.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class SelectUnitState : BattleState
 {
+    int currentUnitIndex = -1;
+
     public override void Enter()
     {
         Debug.Log("SelectUnitState Enter");
         base.Enter();
+
+        StartCoroutine("SelectUnit");
     }
 
+    //TODO: select unit a bit more smart.  this is temp until we get stats up
+    IEnumerator SelectUnit()
+    {
+        currentUnitIndex = (currentUnitIndex + 1) % units.Count;
+        turn.Change(units[currentUnitIndex]);
+        yield return null;
+        owner.ChangeState<CommandSelectionState>();
+    }
+    /*
     protected override void OnMove(object sender, object e)
     {
         Point p = (Point)e;
@@ -26,4 +39,5 @@ public class SelectUnitState : BattleState
             owner.ChangeState<MoveTargetState>();
         }
     }
+    */
 }

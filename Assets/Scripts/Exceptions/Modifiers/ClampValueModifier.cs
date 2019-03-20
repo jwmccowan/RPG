7d9bfc0db01e31c12a1c5e0df1d9e4a828c7803c
John McCowan <jwmccowan@gmail.com>
2019-03-20 00:11:17 -0700

    #28: Value Modifiers
    Here is the implementation of the abstract class.  All pretty simple, just store a value and change anything strung through its Modify method.  Added add, mult, divide, clamp modifiers.  Might add min/max if I need it.  Add will obviously be the main use case, but multiply for crits and divide for protection spells.  Clamp to keep things from going out of hand.

---
+++ Assets/Scripts/Exceptions/Modifiers/ClampValueModifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampValueModifier : ValueModifier
+{
+    public readonly int min;
+    public readonly int max;
+
+    public ClampValueModifier(int sortOrder, int min, int max)
+        : base(sortOrder)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    public override int Modify(int value)
+    {
+        return Mathf.Clamp(value, min, max);
+    }
+}

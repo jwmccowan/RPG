8d0120f81df8d68d38637d635ada254f2d19147e
John McCowan <jwmccowan@gmail.com>
2019-03-19 22:06:16 -0700

    #28: Stat
    Base class for a stat.  It has a value that can be get and set, a base value that stays the same as the game runs unless we do character growth, and a bonus value.  This stat represents what we normally want to grab.  Named after the bonus of ability scores, it might need a rename for clarity.  It normally just returns value, but certain stats might want to return something special.

---
+++ Assets/Scripts/View Model/Actor/Stats/Stat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    public virtual int bonus
+    {
+        get
+        {
+            return value;
+        }
+    }
+
+    public virtual int value
+    {
+        get
+        {
+            return value;
+        }
+        set
+        {
+            this.value = value;
+        }
+    }
+    
+    public virtual int baseValue
+    {
+        get
+        {
+            return baseValue;
+        }
+        set
+        {
+            baseValue = value;
+        }
+    }
+
+    public Stat(int value)
+    {
+        baseValue = value;
+        Reset();
+    }
+
+    // This might be too much power in one hand.
+    // Not really any benefit to it being public, so marking it private.
+    protected virtual void Reset()
+    {
+        value = baseValue;
+    }
+}

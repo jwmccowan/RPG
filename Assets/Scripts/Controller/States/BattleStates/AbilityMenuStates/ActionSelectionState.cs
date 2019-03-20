4292a019ba40e4f14c8c0dbe38dbff63bd1c927d
John McCowan <jwmccowan@gmail.com>
2019-03-19 19:39:20 -0700

    #21: Menu States
    A bunch of states that run the ability menu.  Each inherits from the base menu state, and controls a shared panel (accessed through the controller).  Command Selection State chooses top level commands, Category Selection State chooses the type of action you want, and Action Selection State chooses the action itself.  There is a lot of placeholder code in there until we solidify how we will do abilities, but it runs well enough to know the ability menu works.

---
+++ Assets/Scripts/Controller/States/BattleStates/AbilityMenuStates/ActionSelectionState.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSelectionState : BaseAbilityMenuState
+{
+    public static ActionType actionType;
+    protected override void Cancel()
+    {
+        owner.ChangeState<CategorySelectionState>();
+    }
+
+    protected override void Confirm()
+    {
+        turn.Act(actionType);
+        owner.ChangeState<CommandSelectionState>();
+    }
+
+    protected override void LoadMenu()
+    {
+        if (menuOptions == null)
+        {
+            menuOptions = new List<string>();
+        }
+        menuOptions.Clear();
+        //Here is where we query for possible actions and fill the list
+        //This is all placeholder til we get there
+        switch (actionType)
+        {
+            case ActionType.StandardAction:
+                menuTitle = "Standard";
+                menuOptions.Add("Attack");
+                menuOptions.Add("Magic Missile");
+                menuOptions.Add("Bless");
+                menuOptions.Add("Shield Bash");
+                //And then lock based on the character's situation
+                abilityMenuPanelController.SetLocked(1, true);
+                abilityMenuPanelController.SetLocked(2, true);
+                break;
+            case ActionType.MoveAction:
+                menuTitle = "Move";
+                menuOptions.Add("Draw Weapon");
+                menuOptions.Add("Open Door");
+                menuOptions.Add("Move");
+                break;
+            case ActionType.FullRoundAction:
+                menuTitle = "Full Round";
+                menuOptions.Add("Full Attack");
+                menuOptions.Add("Summon Monster");
+                menuOptions.Add("Run");
+                break;
+            case ActionType.SwiftAction:
+                menuTitle = "Swift";
+                //Gettig lazy but this is all placeholder anyways
+                menuOptions.Add("Swift Action");
+                menuOptions.Add("Swift Action");
+                menuOptions.Add("Swift Action");
+                break;
+            default:
+                Debug.LogError("Action Type not supported.");
+                break;
+        }
+        abilityMenuPanelController.Show(menuTitle, menuOptions);
+    }
+}
---
+++ Assets/Scripts/Controller/States/BattleStates/AbilityMenuStates/BaseAbilityMenuState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseAbilityMenuState : BattleState
+{
+    protected string menuTitle;
+    protected List<string> menuOptions;
+
+    public override void Enter()
+    {
+        base.Enter();
+        SelectTile(turn.actor.tile.pos);
+        LoadMenu();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        abilityMenuPanelController.Hide();
+    }
+
+    protected override void OnFire(object sender, object e)
+    {
+        int i = (int)e;
+        if (i == 0)
+        {
+            Confirm();
+        }
+        else if (i == 1)
+        {
+            Cancel();
+        }
+    }
+
+    protected override void OnMove(object sender, object e)
+    {
+        Point p = (Point)e;
+        if (p.x < 0 || p.y < 0)
+        {
+            abilityMenuPanelController.Next();
+        }
+        else
+        {
+            abilityMenuPanelController.Previous();
+        }
+    }
+
+    protected abstract void LoadMenu();
+    protected abstract void Confirm();
+    protected abstract void Cancel();
+}

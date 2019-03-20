dfc4fb4ccdab650952071c0ea1f2db81c5f83338
John McCowan <jwmccowan@gmail.com>
2019-03-20 00:07:42 -0700

    #28: Exceptions
    Thinking about exceptions as the game gets more complicated.  Need a way to store whether something happens or not.  Here is an easy way to stop something from happening.  Implementation incoming.

---
+++ Assets/Scripts/Exceptions/BaseException.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseException
+{
+    public bool toggle { get; private set; }
+    bool defaultToggle;
+
+    public BaseException(bool defaultToggle)
+    {
+        this.defaultToggle = defaultToggle;
+    }
+
+    public void FlipToggle()
+    {
+        toggle = !defaultToggle;
+    }
+}

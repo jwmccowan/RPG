4e1b97edb14c0f95c57f94fcfb0c39776f5b5046
John McCowan <jwmccowan@gmail.com>
2019-03-20 00:08:58 -0700

    #28: Modifier
    A abstract class that represents something that needs to change.  We can attach these to listeners so that stats can change without changing the character sheet

---
+++ Assets/Scripts/Exceptions/Modifiers/Modifier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Modifier
+{
+    public readonly int sortOrder;
+
+    public Modifier(int sortOrder)
+    {
+        this.sortOrder = sortOrder;
+    }
+}

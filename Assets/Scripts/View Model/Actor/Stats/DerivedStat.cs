d572e4e4f1684c973f36155348159b487f2147b1
John McCowan <jwmccowan@gmail.com>
2019-03-19 22:08:19 -0700

    #28: Derived Stat
    contains a list of 'base stats' and adds those values before returning its bonus.  To be used for AC, skills, HP, etc

---
+++ Assets/Scripts/View Model/Actor/Stats/DerivedStat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerivedStat : Stat
+{
+    List<Stat> baseStats;
+
+    public override int bonus
+    {
+        get
+        {
+            int retValue = value;
+            foreach(Stat s in baseStats)
+            {
+                retValue += s.bonus;
+            }
+            return retValue;
+        }
+    }
+
+    public DerivedStat(int value, List<Stat> baseStats)
+        : base(value)
+    {
+        this.baseStats = new List<Stat>();
+        for (int i = 0; i < baseStats.Count; i++)
+        {
+            this.baseStats.Add(baseStats[i]);
+        }
+    }
+}

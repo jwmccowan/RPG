2344312cfdf3004f27d7ce3b0c88c80b35716bc0
John McCowan <jwmccowan@gmail.com>
2019-03-21 23:39:35 -0700

    #28: More StatTypes
    Add a few more stats to reflect changes to architecture and new features

--- Assets/Scripts/View Model/Actor/Stats/StatTypes.cs
+++ Assets/Scripts/View Model/Actor/Stats/StatTypes.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public enum StatTypes
 {
+    Level,
+    Experience,
     HP,
     Max_HP,
     HP_Increases,
+    HP_Rolled,
     Strength,
+    Strength_Bonus,
     Dexterity,
+    Dexterity_Bonus,
     Intelligence,
+    Intelligence_Bonus,
     AC,
+    Initiative_Ranks,
     Initiative,
     BAB,
+    To_Hit,
 
     Count
 }

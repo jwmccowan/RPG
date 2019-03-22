3d442b140c5ba0ecd91c5f44550a352a72a5dda0
John McCowan <jwmccowan@gmail.com>
2019-03-21 23:32:45 -0700

    #28: Delta Modifiers
    allow multiplication and clamp for deltas of values, for things like exp gain

---
+++ Assets/Scripts/Exceptions/Modifiers/ClampDeltaValueModifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampDeltaValueModifier : ValueModifier
+{
+    public readonly int min;
+    public readonly int max;
+
+    public ClampDeltaValueModifier(int sortOrder, int min, int max)
+        : base(sortOrder)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    public override int Modify(int fromValue, int toValue)
+    {
+        int delta = toValue - fromValue;
+        return fromValue + Mathf.Clamp(delta, min, max);
+    }
+}
---
+++ Assets/Scripts/Exceptions/Modifiers/MultDeltaValueModifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultDeltaValueModifier : ValueModifier
+{
+    public readonly int toMultiply;
+
+    public MultDeltaValueModifier(int sortOrder, int toMultiply)
+        : base(sortOrder)
+    {
+        this.toMultiply = toMultiply;
+    }
+
+    public override int Modify(int fromValue, int toValue)
+    {
+        int delta = toValue - fromValue;
+        return fromValue + toMultiply * delta;
+    }
+}

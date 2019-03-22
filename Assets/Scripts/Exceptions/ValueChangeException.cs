878514550cd504a92a44ad128f615b9b427bc14a
John McCowan <jwmccowan@gmail.com>
2019-03-21 23:40:40 -0700

    #28: ValueChangeException
    To be passed along with stats being changed.  Combines modifiers and applies them before spitting out the new final value

---
+++ Assets/Scripts/Exceptions/ValueChangeException.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueChangeException : BaseException
+{
+    #region fields
+    public readonly int fromValue;
+    public readonly int toValue;
+    public int delta { get { return toValue - fromValue; } }
+    List<ValueModifier> modifiers;
+    #endregion
+
+    #region Constructor
+    public ValueChangeException(int fromValue, int toValue)
+        : base(true)
+    {
+        this.fromValue = fromValue;
+        this.toValue = toValue;
+    }
+    #endregion
+
+    #region public
+    public void AddModifier(ValueModifier m)
+    {
+        if (modifiers == null)
+        {
+            modifiers = new List<ValueModifier>();
+        }
+        modifiers.Add(m);
+    }
+
+    public int GetModifiedValue()
+    {
+        if (modifiers == null)
+        {
+            return toValue;
+        }
+        int retValue = toValue;
+
+        modifiers.Sort(Compare);
+        for (int i = 0; i < modifiers.Count; i++)
+        {
+            retValue = modifiers[i].Modify(fromValue, retValue);
+        }
+        return retValue;
+    }
+    #endregion
+
+    #region private
+    int Compare(ValueModifier a, ValueModifier b)
+    {
+        return a.sortOrder.CompareTo(b.sortOrder);
+    }
+    #endregion
+}

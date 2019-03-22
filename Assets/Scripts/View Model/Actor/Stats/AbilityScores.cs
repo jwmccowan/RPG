9b8ee5788c8874794b2fefd0391a6c6c8fca0c25
John McCowan <jwmccowan@gmail.com>
2019-03-22 12:55:41 -0700

    #28: AbilityScores.cs
    Initial commit for AbilityScores

---
+++ Assets/Scripts/View Model/Actor/Stats/AbilityScores.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour
+{
+    #region constants
+    static StatTypes[] abilityScores = new StatTypes[6] 
+    {
+        StatTypes.Strength,
+        StatTypes.Dexterity,
+        StatTypes.Constitution,
+        StatTypes.Intelligence,
+        StatTypes.Wisdom,
+        StatTypes.Charisma
+    };
+    #endregion
+
+    #region fields
+    Stats stats;
+    #endregion
+
+    #region MonoBehaviour
+    private void Awake()
+    {
+        stats = GetComponent<Stats>();
+    }
+    #endregion
+
+    #region events
+
+    #endregion
+
+    #region public
+
+    #endregion
+
+    #region private
+
+    #endregion
+}

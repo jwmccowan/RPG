1ed3d9adc606391b78c80d80a37c4047ede5a9ff
John McCowan <jwmccowan@gmail.com>
2019-03-22 12:57:18 -0700

    #28: CharacterSheet
    Collection of stat with logic to keep bonus and skills in sync

---
+++ Assets/Scripts/View Model/Actor/Stats/CharacterSheet.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSheet : MonoBehaviour
+{
+    #region constants
+    static Dictionary<StatTypes, StatTypes> abilityScoresBonuses = new Dictionary<StatTypes, StatTypes>()
+    {
+        { StatTypes.Strength, StatTypes.Strength_Bonus },
+        { StatTypes.Dexterity, StatTypes.Dexterity_Bonus },
+        { StatTypes.Constitution, StatTypes.Constitution_Bonus },
+        { StatTypes.Intelligence, StatTypes.Intelligence_Bonus },
+        { StatTypes.Wisdom, StatTypes.Wisdom_Bonus },
+        { StatTypes.Charisma, StatTypes.Charisma_Bonus }
+    };
+    #endregion
+
+    #region fields
+    Stats stats;
+    #endregion
+
+    #region MonoBehaviour
+    void OnEnable()
+    {
+        this.AddListener(OnAbilityScoreDidChange, Stats.DidChangeNotification(StatTypes.Strength), stats);
+        this.AddListener(OnAbilityScoreDidChange, Stats.DidChangeNotification(StatTypes.Dexterity), stats);
+        this.AddListener(OnAbilityScoreDidChange, Stats.DidChangeNotification(StatTypes.Constitution), stats);
+        this.AddListener(OnAbilityScoreDidChange, Stats.DidChangeNotification(StatTypes.Intelligence), stats);
+        this.AddListener(OnAbilityScoreDidChange, Stats.DidChangeNotification(StatTypes.Wisdom), stats);
+        this.AddListener(OnAbilityScoreDidChange, Stats.DidChangeNotification(StatTypes.Charisma), stats);
+    }
+
+    void Awake()
+    {
+        stats = GetComponent<Stats>();
+    }
+    #endregion
+
+    #region events
+    void OnAbilityScoreDidChange(object sender, object e)
+    {
+        // We set all bonuses any time any ability score changes.  This is to save on code space.
+        // Since we don't change ability scores that often, it seems a decent tradeoff.
+        // If I see this to be a significant part to optimize, it should be done ASAP
+        foreach (KeyValuePair<StatTypes, StatTypes> pair in abilityScoresBonuses)
+        {
+            SetBonus(pair.Key);
+        }
+    }
+    #endregion
+
+    #region public
+
+    #endregion
+
+    #region private
+    void SetBonus(StatTypes s)
+    {
+        if (abilityScoresBonuses.ContainsKey(s))
+        {
+            int bonus = Mathf.FloorToInt((stats[s] - 10) / 2);
+            stats.SetValue(abilityScoresBonuses[s], bonus, false);
+        }
+        else
+        {
+            Debug.LogError(string.Format("There isn't a corresponding ability bonus for type: {0}", s.ToString()));
+        }   
+    }
+    #endregion
+}

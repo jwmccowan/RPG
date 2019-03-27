82bf75256cf0dcabe8b50f9fe91a56efc1a10858
jwmccowan <jwmccowan@gmail.com>
2019-03-27 03:07:08 -0700

    #30: Equipment
    Add an equipment component, to be added to units.  It controls the equipping and unequipping of equippables.

---
+++ Assets/Scripts/Enums/EquipSlots.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Flags]
+public enum EquipSlots
+{
+    None = 0,
+    Belts = 1 << 0,
+    Body = 1 << 1,
+    Chest = 1 << 2,
+    Eyes = 1 << 3,
+    Feet = 1 << 4,
+    Hand = 1 << 5,
+    Head = 1 << 6,
+    Headband = 1 << 7,
+    Neck = 1 << 8,
+    RingLeft = 1 << 9,
+    RingRight = 1 << 10,
+    Shield = 1 << 11,
+    Shoulders = 1 << 12,
+    Wrist = 1 << 13,
+    Armor = 1 << 14,
+    OffHand = 1 << 15
+}
---
+++ Assets/Scripts/View Model/Actor/Equipment.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+    #region constants
+    public const string equippedNotification = "Equipment.EquippedNotification";
+    public const string unequippedNotification = "Equipment.UnequippedNotification";
+    #endregion
+
+    #region fields
+    public IList <Equippable> items { get { return itemList.AsReadOnly(); } }
+    public List<Equippable> itemList = new List<Equippable>();
+    #endregion
+
+    #region public
+    public void Equip(Equippable item, EquipSlots slots)
+    {
+        Unequip(slots);
+
+        itemList.Add(item);
+        item.transform.SetParent(transform);
+        item.slots = slots;
+        item.OnEquip();
+
+        this.PostNotification(equippedNotification, item);
+    }
+
+    public void Unequip(Equippable item)
+    {
+        item.OnUnEquip();
+        item.slots = EquipSlots.None;
+        item.transform.SetParent(transform);
+        itemList.Remove(item);
+
+        this.PostNotification(unequippedNotification, item);
+    }
+
+    public void Unequip(EquipSlots slots)
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            if ((itemList[i].slots & slots) != EquipSlots.None)
+            {
+                Unequip(itemList[i]);
+            }
+        }
+    }
+    #endregion
+}

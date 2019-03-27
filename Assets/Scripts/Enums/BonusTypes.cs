194f95656040f3fdb071dfa00642644c774af3b4
jwmccowan <jwmccowan@gmail.com>
2019-03-27 03:03:42 -0700

    #30: Bonus
    Bonus contains a bonus type, a value, and the stat it modifies.  It is passed by a stat bonus modifier to the character sheet, which makes sure they do not stack.

---
+++ Assets/Scripts/Enums/BonusTypes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BonusTypes
+{
+    Armor,
+    Circumstance,
+    Deflection,
+    Dodge,
+    Enhancement,
+    Inherent,
+    Luck,
+    NaturalArmor,
+    Racial,
+    Resistance,
+    Shield,
+    Size,
+    Trait,
+    Count
+}

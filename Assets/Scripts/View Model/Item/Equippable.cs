cd8f222c6946782cd496ef29098e16cfb056eb6f
jwmccowan <jwmccowan@gmail.com>
2019-03-27 02:59:01 -0700

    #30: Item Components
    Added 3 components to be added to items in game.  Equippable, Consumable, and Merchandise.

---
+++ Assets/Scripts/View Model/Item/Equippable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equippable : MonoBehaviour
+{
+    public EquipSlots primarySlot;
+    public EquipSlots secondarySlot;
+    public EquipSlots slots;
+    bool isEquipped;
+
+    public void OnEquip()
+    {
+        if (isEquipped)
+        {
+            return;
+        }
+        isEquipped = true;
+
+        Feature[] features = GetComponentsInChildren<Feature>();
+        for (int i = 0; i < features.Length; i++)
+        {
+            features[i].Activate(gameObject);
+        }
+    }
+
+    public void OnUnEquip()
+    {
+        if (!isEquipped)
+        {
+            return;
+        }
+        isEquipped = false;
+
+        Feature[] features = GetComponentsInChildren<Feature>();
+        for (int i = 0; i < features.Length; i++)
+        {
+            features[i].Deactivate();
+        }
+    }
+}
---
+++ Assets/Scripts/View Model/Item/Merchandise.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchandise : MonoBehaviour
+{
+    public int buy;
+    public int sell;
+}

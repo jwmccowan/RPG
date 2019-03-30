5938a5af7fefcf5d6796811bb00de3e15a6c1ad2
jwmccowan <jwmccowan@gmail.com>
2019-03-30 02:16:32 -0700

    #33: Bonus Feature
    Now has reference to bonus handler instead of character sheet, coming from previous refactor

--- Assets/Scripts/View Model/Feature/StatBonusFeature.cs
+++ Assets/Scripts/View Model/Feature/StatBonusFeature.cs
@@ -7,18 +7,18 @@ public class StatBonusFeature : Feature
     #region fields
     public Bonus bonus;
 
-    CharacterSheet characterSheet { get { return target.GetComponentInParent<CharacterSheet>(); } }
+    BonusHandler bonusHandler { get { return target.GetComponentInParent<BonusHandler>(); } }
     #endregion
 
     #region protected
     protected override void OnApply()
     {
-        characterSheet.AddBonus(bonus);
+        bonusHandler.AddBonus(bonus);
     }
 
     protected override void OnRemove()
     {
-        characterSheet.RemoveBonus(bonus);
+        bonusHandler.RemoveBonus(bonus);
     }
     #endregion
 }

19c05643c1fbb0f0ed6be7771c4dfc9344d8f6bd
jwmccowan <jwmccowan@gmail.com>
2019-04-01 21:30:13 -0700

    #46: Ability Area of Effect
    Ability area of effect decides which units are affected by an ability.  Added specify ability (eg fireball), unit ability (eg arrow), and full, which affects the entire range

---
+++ Assets/Scripts/View Model/Actor/Abilities/Ability AOE/UnitAbilityArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAbilityArea : AbilityArea
+{
+    public override List<Tile> GetTilesInArea(Board board, Point pos)
+    {
+        List<Tile> retValue = new List<Tile>(1);
+        Tile t = board.GetTile(pos);
+        if (t != null)
+        {
+            retValue.Add(t);
+        }
+        return retValue;
+    }
+}

9d37e95b8d5d15e24515e350cddb6400e7e4cecd
jwmccowan <jwmccowan@gmail.com>
2019-04-01 18:54:34 -0700

    #44: Ability Ranges
    Concrete implementations of ability ranges.  Types included are those described in the ticket

---
+++ Assets/Scripts/View Model/Actor/Abilities/Ability Range/ConstantAbilityRange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantAbilityRange : AbilityRange
+{
+    public override List<Tile> GetTilesInRange(Board board)
+    {
+        return board.Search(unit.tile, ExpandSearch);
+    }
+
+    bool ExpandSearch(Tile from, Tile to)
+    {
+        return (from.distance + 1) <= horizontal && Mathf.Abs(to.height - from.height) <= vertical;
+    }
+}
---
+++ Assets/Scripts/View Model/Actor/Abilities/Ability Range/InfiniteAbilityRange.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteAbilityRange : AbilityRange
+{
+    public override List<Tile> GetTilesInRange(Board board)
+    {
+        return new List<Tile>(board.tiles.Values);
+    }
+}

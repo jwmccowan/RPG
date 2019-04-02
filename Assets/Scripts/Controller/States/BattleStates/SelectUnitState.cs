6c78332aa7cf808cec746360b8705362fc6d633e
jwmccowan <jwmccowan@gmail.com>
2019-04-02 02:28:26 -0700

    #49: States
    Select Unit state now runs MoveNext on initiative controller, which selects next unit.  Small changes to other states to support

--- Assets/Scripts/Controller/States/BattleStates/SelectUnitState.cs
+++ Assets/Scripts/Controller/States/BattleStates/SelectUnitState.cs
@@ -23,8 +23,8 @@ public class SelectUnitState : BattleState
     //TODO: select unit a bit more smart.  this is temp until we get stats up
     IEnumerator SelectUnit()
     {
-        currentUnitIndex = (currentUnitIndex + 1) % units.Count;
-        turn.Change(units[currentUnitIndex]);
+        owner.round.MoveNext();
+        SelectTile(turn.actor.tile.pos);
         RefreshPrimaryStatPanel(pos);
         yield return null;
         owner.ChangeState<CommandSelectionState>();

bc806ec04e49d484eff50c051b164db3f687a2df
jwmccowan <jwmccowan@gmail.com>
2019-04-03 12:21:18 -0700

    #52: Status Condition
    A status condition represents whether a status effect is currently applied to a unit.  A base status condition really just knows how to unhook itself from a unit’s status.  The duration status condition will do so after a certain number of turns.

---
+++ Assets/Scripts/View Model/Actor/Status/Condition/DurationStatusCondition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DurationStatusCondition : StatusCondition
+{
+    public int duration = 10;
+    Unit unit;
+
+    private void OnEnable()
+    {
+        unit = GetComponentInParent<Unit>();
+        this.AddListener(OnTurnBegan, InitiativeController.TurnBeganNotification, unit);
+    }
+
+    private void OnDisable()
+    {
+        this.RemoveListener(OnTurnBegan, InitiativeController.TurnBeganNotification, unit);
+    }
+
+    void OnTurnBegan(object sender, object e)
+    {
+        duration--;
+        if (duration <= 0)
+        {
+            Remove();
+        }
+    }
+}

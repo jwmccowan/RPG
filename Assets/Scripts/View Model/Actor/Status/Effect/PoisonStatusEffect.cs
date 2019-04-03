50b7d8600b82a858248c2508857d5a200b52456c
jwmccowan <jwmccowan@gmail.com>
2019-04-03 12:24:04 -0700

    #52: Status Effect
    A status effect represents the stats (or effect) a status condition has.  Included are a poison effect, which damages HP at the start of turn, and bless, which raises to hit and will save by one.

---
+++ Assets/Scripts/View Model/Actor/Status/Effect/PoisonStatusEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonStatusEffect : StatusEffect
+{
+    Unit unit;
+    public Roll roll;
+
+    void OnEnable()
+    {
+        unit = GetComponentInParent<Unit>();
+        if (roll == null) roll = new Roll(4);
+        this.AddListener(OnTurnBegan, InitiativeController.TurnBeganNotification, unit);
+    }
+
+    private void OnDisable()
+    {
+        this.RemoveListener(OnTurnBegan, InitiativeController.TurnBeganNotification, unit);
+    }
+
+    void OnTurnBegan(object sender, object e)
+    {
+        CharacterSheet sheet = GetComponentInParent<CharacterSheet>();
+        int damage = roll.NewRoll();
+        Debug.Log(string.Format("Doing {0} points of poison damage", damage));
+        sheet.stats.SetValue(StatTypes.HP, sheet.stats[StatTypes.HP] - damage, false);
+    }
+}

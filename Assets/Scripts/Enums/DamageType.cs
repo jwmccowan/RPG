4abcfc5ac426146e404566a860c4a154000731ab
jwmccowan <jwmccowan@gmail.com>
2019-06-30 19:44:46 -0700

    #56: Damage Type
    Just an enum representing damage types

---
+++ Assets/Scripts/Enums/DamageType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DamageType
+{
+    Bludgeoning,
+    Piercing,
+    Slashing,
+    Fire,
+    Cold,
+    Acid,
+    Electricity,
+    Untyped,
+    Count
+}

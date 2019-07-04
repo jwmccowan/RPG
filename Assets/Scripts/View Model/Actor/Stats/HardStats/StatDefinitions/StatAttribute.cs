d59e081350a39a12685ebce254af4cf19a142c51
John McCowan <jwmccowan@gmail.com>
2019-07-04 03:15:20 -0700

    #59: IStatAbilityScorePercent
    Created an interface to allow an ability score bonus to add a flat percent to an attribute, and modified StatAttribute accordingly

--- Assets/Scripts/View Model/Actor/Stats/HardStats/StatDefinitions/StatAttribute.cs
+++ Assets/Scripts/View Model/Actor/Stats/HardStats/StatDefinitions/StatAttribute.cs
@@ -8,34 +8,39 @@ using UnityEngine;
 /// attributes per level, but as a means of implementing as many stat
 /// types as possible, here we are
 /// </summary>
-public class StatAttribute : StatModifiable, IStatScalable, IStatLinkable
+public class StatAttribute : StatModifiable, IStatScalable, IStatLinkable, IStatAbilityScorePercentModifiable
 {
     private float _statLevelValue;
     private float _statLinkerValue;
+    private float _statAbilityScoreBonus;
 
     private List<StatLinker> statLinkers;
+    private Dictionary<Stat, float> abilityScoreModifiers;
+
+    public override float statBaseValue
+    {
+        get { return (base.statBaseValue + statLevelValue + statLinkerValue) * (1 + statAbilityScoreBonus); }
+    }
 
     public float statLevelValue
     {
         get { return _statLevelValue; }
     }
 
-    public override float statBaseValue
-    {
-        get { return base.statBaseValue + statLevelValue + statLinkerValue; }
-    }
-
     public float statLinkerValue
     {
-        get
-        {
-            return _statLinkerValue;
-        }
+        get { return _statLinkerValue; }
+    }
+
+    public float statAbilityScoreBonus
+    {
+        get { return _statAbilityScoreBonus; }
     }
 
     public StatAttribute()
     {
         statLinkers = new List<StatLinker>();
+        abilityScoreModifiers = new Dictionary<Stat, float>();
     }
 
     public void AddLinker(StatLinker statLinker)
@@ -62,8 +67,11 @@ public class StatAttribute : StatModifiable, IStatScalable, IStatLinkable
 
     public void ScaleStat(int level)
     {
-        _statLevelValue = level;
-        PostValueDidChange();
+        if (_statLevelValue != level)
+        {
+            _statLevelValue = level;
+            PostValueDidChange();
+        }
     }
 
     private void OnLinkedStatValueChanged(object sender, object e)
@@ -73,12 +81,58 @@ public class StatAttribute : StatModifiable, IStatScalable, IStatLinkable
 
     public void UpdateLinkers()
     {
+        float oldStatLinkerValue = _statLinkerValue;
         _statLinkerValue = 0f;
         for (int i = 0; i < statLinkers.Count; i++)
         {
             _statLinkerValue += statLinkers[i].value;
         }
-        PostValueDidChange();
+        if (oldStatLinkerValue != _statLinkerValue)
+        {
+            PostValueDidChange();
+        }
+    }
+
+    public void AddAbilityScorePercentModifier(Stat abilityScore, float ratio)
+    {
+        if (abilityScoreModifiers.ContainsKey(abilityScore))
+        {
+            ChangeAbilityScorePercentModifier(abilityScore, ratio);
+        }
+        abilityScoreModifiers.Add(abilityScore, ratio);
+        this.AddListener(OnAbilityScoreChanged, StatCollection.StatValueDidChangeNotification, abilityScore);
+        UpdateAbilityScoreModifiers();
+    }
+
+    public void ChangeAbilityScorePercentModifier(Stat abilityScore, float ratio)
+    {
+        abilityScoreModifiers[abilityScore] = ratio;
+        UpdateAbilityScoreModifiers();
+    }
+
+    public void RemoveAbilityScorePercentModifier(Stat abilityScore)
+    {
+        if (abilityScoreModifiers.ContainsKey(abilityScore))
+        {
+            abilityScoreModifiers.Remove(abilityScore);
+        }
+        this.RemoveListener(OnAbilityScoreChanged, StatCollection.StatValueDidChangeNotification, abilityScore);
+        UpdateAbilityScoreModifiers();
+    }
+
+    public void UpdateAbilityScoreModifiers()
+    {
+        _statAbilityScoreBonus = 0f;
+        foreach (var key in abilityScoreModifiers.Keys)
+        {
+            _statAbilityScoreBonus += (abilityScoreModifiers[key] * (key.statValue - 10f));
+        }
+        Debug.Log("eggs " + _statAbilityScoreBonus);
+    }
+
+    public void OnAbilityScoreChanged(object sender, object e)
+    {
+        UpdateAbilityScoreModifiers();
     }
 
     private void PostValueDidChange()

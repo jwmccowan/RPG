d3ae6b252c0b598fc0ab7cb6c0bf6675868e41ef
John McCowan <jwmccowan@gmail.com>
2019-07-04 03:13:04 -0700

    #59: AbilityScore Linker
    Made a linker that adds a percent of an ability score bonus, defined as the score -10

---
+++ Assets/Scripts/View Model/Actor/Stats/HardStats/Utilities/StatLinkerAbilityScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatLinkerAbilityScore : StatLinker
+{
+    private float ratio;
+
+    public override float value
+    {
+        get {
+            float abilityScoreBonus = Mathf.Floor(stat.statValue - 10);
+            return abilityScoreBonus * ratio;
+        }
+    }
+
+    public StatLinkerAbilityScore(Stat stat, float ratio)
+        : base(stat)
+    {
+        this.ratio = ratio;
+    }
+}

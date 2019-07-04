bb6d33d2f0dffbdf9a354ca564982e3919e289ac
John McCowan <jwmccowan@gmail.com>
2019-07-03 19:12:06 -0700

    #59: StatAttribute
    StatAttribute is closer to an official final stat.  It is scalable by level (by implementing IStatScalable interface) and can be linked to another stat (by implementing IStatLinkable).  Linking increases the stat by another stats value (multiplied by a ratio value).

---
+++ Assets/Scripts/View Model/Actor/Stats/HardStats/StatDefinitions/StatAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// For now, the Attribute type means that you gain a point every level.
+/// I don't think this is going to be permanent, as I don't plan to gain 
+/// attributes per level, but as a means of implementing as many stat
+/// types as possible, here we are
+/// </summary>
+public class StatAttribute : StatModifiable, IStatScalable, IStatLinkable
+{
+    private float _statLevelValue;
+    private float _statLinkerValue;
+
+    private List<StatLinker> statLinkers;
+
+    public float statLevelValue
+    {
+        get { return _statLevelValue; }
+    }
+
+    public override float statBaseValue
+    {
+        get { return base.statBaseValue + statLevelValue + statLinkerValue; }
+    }
+
+    public float statLinkerValue
+    {
+        get
+        {
+            UpdateLinkers();
+            return _statLinkerValue;
+        }
+    }
+
+    public StatAttribute()
+    {
+        statLinkers = new List<StatLinker>();
+    }
+
+    public void AddLinker(StatLinker statLinker)
+    {
+        statLinkers.Add(statLinker);
+    }
+
+    public void ClearLinkers()
+    {
+        statLinkers.Clear();
+    }
+
+    public void ScaleStat(int level)
+    {
+        _statLevelValue = level;
+    }
+
+    public void UpdateLinkers()
+    {
+        _statLinkerValue = 0f;
+        for (int i = 0; i < statLinkers.Count; i++)
+        {
+            _statLinkerValue += statLinkers[i].value;
+        }
+    }
+}
---
+++ Assets/Scripts/View Model/Actor/Stats/HardStats/Utilities/StatLinker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatLinker
+{
+    private Stat _stat;
+
+    public Stat stat
+    {
+        get { return _stat; }
+    }
+
+    public abstract float value { get; }
+
+    public StatLinker(Stat stat)
+    {
+        this._stat = stat;
+    }
+}
